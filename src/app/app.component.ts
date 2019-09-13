b621726a5c13b0bd9cadbc581d7d1069b8601fcb
MoahNonsense <moah.nonsense@gmail.com>
2019-09-13 04:35:28 +0200

    code-challenge stay-firends complete

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from '@angular/core';
+import {StatesService} from './services/states.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Testaufgabe - Bundesland';
+  states = {};
+  personCount = {};
+  persons = [];
+
+  constructor(private statesServiceService: StatesService) {}
+  ngOnInit() {
+    /// following code is the best practice approach if the request to the api is a success
+    // this.statesServiceService.getStates()
+    //   .subscribe(response => {
+    //     this.states = response;
+    //   });
+    // tslint:disable-next-line:forin
+    // for (const person in this.states.personIndexCounts) {
+    //   const currentPersonCount = {
+    //     name: person,
+    //     count: this.states.personIndexCounts[person]
+    //   }
+    //   if (this.states.personIndexCounts[person] > 0) {
+    //     this.persons.push(currentPersonCount);
+    //   }
+    // };
+    /// fetching mocked data from states.service -> statesData object
+    this.states = this.statesServiceService.getStates();
+    this.personCount = this.statesServiceService.getPersonIndexCount();
+    // tslint:disable-next-line:forin
+    for (const person in this.personCount) {
+      const currentPersonCount = {
+        name: person,
+        count: this.personCount[person]
+      }
+      if (this.personCount[person] > 0) {
+        this.persons.push(currentPersonCount);
+      }
+    };
+  }
+}
